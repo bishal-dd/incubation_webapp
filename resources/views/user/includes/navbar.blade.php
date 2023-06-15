5b655771a0e6d0b540aafbfd488660b8e7e07b3e
Bishal Dhakal <dhakalbishal930@.com>
2023-06-15 14:34:34 +0600

    first commit

---
+++ resources/views/user/includes/navbar.blade.php
@@ -0,0 +1,106 @@
+@include('user.includes.loader')
+        <!-- Start header -->
+        <header id="header" class="site-header header-style-3">
+            <nav class="navigation navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="open-btn">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index-2.html"><img src="assets/images/logo-2.png" alt></a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse navigation-holder">
+                        <button class="close-navbar"><i class="ti-close"></i></button>
+                        <ul class="nav navbar-nav">
+                            <li class="menu-item-has-children">
+                                <a href="#">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index-2.html">Home Default</a></li>
+                                    <li><a href="index-3.html">Home style 2</a></li>
+                                    <li><a href="index-4.html">Home style 3</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="about.html">About</a></li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                    <li><a href="testimonials.html">Testimonials</a></li>
+                                    <li><a href="testimonials-s2.html">Testimonials style 2</a></li>
+                                    <li><a href="team.html">Team</a></li>
+                                    <li><a href="faq.html">FAQ</a></li>
+                                    <li><a href="404.html">404</a></li>
+                                    <li><a href="shop.html">Shop</a></li>
+                                    <li><a href="shop-single.html">Shop single</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Services</a>
+                                <ul class="sub-menu">
+                                    <li><a href="services.html">Services</a></li>
+                                    <li><a href="services-s2.html">Services style 2</a></li>
+                                    <li><a href="service-single.html">Mechanical Engineering</a></li>
+                                    <li><a href="oil-gas.html">Oil And Gas</a></li>
+                                    <li><a href="power-energy.html">Power And Energy</a></li>
+                                    <li><a href="sanitary-plumbing.html">Sanitary & Plumbing</a></li>
+                                    <li><a href="electrical-power.html">Electrical Power</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Projects</a>
+                                <ul class="sub-menu">
+                                    <li><a href="portfolio.html">Projects</a></li>
+                                    <li><a href="portfolio-s2.html">Projects style 2</a></li>
+                                    <li><a href="project-single.html">Project single</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-left-sidebar.html">Blog left sidebar</a></li>
+                                    <li><a href="blog-fullwidth.html">Blog full width</a></li>
+                                    <li class="menu-item-has-children">
+                                        <a href="#Level3">Blog single</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="blog-single.html">Blog single</a></li>
+                                            <li><a href="blog-single-left-sidebar.html">Blog single left sidebar</a></li>
+                                            <li><a href="blog-single-fullwidth.html">Blog single full width</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div><!-- end of nav-collapse -->
+
+                    <div class="search-contact">
+                        <div class="header-search-area">
+                            <div class="header-search-form">
+                                <form class="form">
+                                    <div>
+                                        <input type="text" class="form-control" placeholder="Search here">
+                                    </div>
+                                    <button type="submit" class="btn"><i class="ti-search"></i></button>
+                                </form>
+                            </div>
+                            <div>
+                                <button class="btn open-btn"><i class="ti-search"></i></button>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <div class="call">
+                                <i class="fi flaticon-call"></i>
+                                <p>Call us anytime</p>
+                                <h5>+65487441584</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end of container -->
+            </nav>
+        </header>
+        <!-- end of header -->
