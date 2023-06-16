0fd2644833f7712ffe3a628a0aea363a8fdaff8a
Sangay607 <sangaypelzang6070@gmail.com>
2023-06-16 13:42:24 +0530

    changes text

--- resources/views/user/advisory/advisory.blade.php
+++ resources/views/user/advisory/advisory.blade.php
@@ -20,8 +20,7 @@
                 <div class="row">
                     <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
                         <div class="section-title-s5">
-                            <span>Our Team</span>
-                            <h2>Dedicated Team</h2>
+                            <h2>Incubation Advisory Board</h2>
                             <p>Hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him</p>
                         </div>
                     </div>
@@ -33,14 +32,6 @@
                                 <div class="img-social">
                                     <div class="img-holder">
                                         <img src="assets/images/team/img-1.jpg" alt>
-                                    </div>
-                                    <div class="social">
-                                        <ul>
-                                            <li><a href="#"><i class="ti-facebook"></i></a></li>
-                                            <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
-                                            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                                        </ul>
                                     </div>
                                 </div>
                                 <div class="details">
