401a7bfb263f73184d144dce6d351dea66a06c2e
Bishal Dhakal <dhakalbishal930@.com>
2023-06-16 15:22:19 +0600

    login spelling fixed

--- app/Http/Controllers/AuthController/LoginController.php
+++ app/Http/Controllers/AuthController/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
     {
         $request->validate([
             "username" => "required",
-            "password" => "requried",
+            "password" => "required",
         ]);
 
         dd($request->all());
