1b45c9a676dd33da84408d3246f5638fd0f109ea
Bishal Dhakal <dhakalbishal930@.com>
2023-06-16 10:27:15 +0600

    changes

--- resources/views/admin/common/navbar.blade.php
+++ resources/views/admin/common/navbar.blade.php
@@ -241,7 +241,7 @@
         </a>
         <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="forms-elements.html">
+            <a href="/dashboard/add_slider_image">
               <i class="bi bi-circle"></i><span>Add Slider Image</span>
             </a>
           </li>
--- resources/views/user/login/login.blade.php
+++ resources/views/user/login/login.blade.php
@@ -3,22 +3,21 @@
 
 <!-- start page-title -->
 <section class="page-title">
-    <div class="container">
-      <div class="row">
-        <div class="col col-xs-12">
-          <h2>Login</h2>
-        </div>
-      </div>
-      <!-- end row -->
-    </div>
-    <!-- end container -->
-  </section>
-  <!-- end page-title -->
-
   <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-6"> 
-        <h2 class="text-center mb-4">Login</h2>
+    <div class="row">
+      <div class="col col-xs-12">
+        <h2>Login</h2>
+      </div>
+    </div>
+    <!-- end row -->
+  </div>
+  <!-- end container -->
+</section>
+<!-- end page-title -->
+
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+  <div style="width: 400px; padding: 20px; border: 1px solid #ccc; border-radius: 5px;">
+    <h2 style="text-align: center; margin-bottom: 20px;">Login</h2>
     <form>
       <div class="form-group">
         <label for="username">Username</label>
@@ -29,8 +28,8 @@
         <input type="password" class="form-control" id="password" placeholder="Enter your password" required>
       </div>
       <button type="submit" class="btn btn-primary btn-block">Login</button>
-    </form></div></div>
-   
+    </form>
   </div>
+</div>
 
 @include('user.includes.js')
